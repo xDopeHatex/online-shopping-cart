7196c1cd2630e1e31a5e8bbf1ba54a2a699bf8e2
Ivan Gorokhov <xgorokhovx@gmail.com>
2023-05-05 23:30:33 +0300

    feat: add delete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import Navbar from "./components/Navbar";
+import CardContainer from "./components/CardContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
+
 function App() {
-  return <h2>Redux Toolkit</h2>;
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  return (
+    <main>
+      <Navbar></Navbar>
+      <CardContainer></CardContainer>
+    </main>
+  );
 }
 export default App;
---
+++ src/components/CardContainer.js
@@ -0,0 +1,45 @@
+import CardItem from "./CardItem";
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+
+function CardContainer() {
+  const dispatch = useDispatch();
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CardItem key={item.id} {...item}></CardItem>;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>${total.toFixed(2)}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
+      </footer>
+    </section>
+  );
+}
+
+export default CardContainer;
